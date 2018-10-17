2033131cf5a404ce7086582959fbb45fd931fb27
macuduranti <macuduranti@gmail.com>
2018-10-17 00:04:18 -0300

    todo

--- scripts/ResponsivePage.user.js
+++ scripts/ResponsivePage.user.js
@@ -123,65 +123,53 @@
 
 	function importJson() {
 		var myUrl = window.location.href;
-        window.fetch("http://localhost:3000/api/augmentations/?url=" + myUrl)
-		.then(function(response) {
-            response.json().then(function(data) {
-                // data es el resultado
-                console.log(data);
-                console.log(JSON.stringify(data));
-                if (data.includes("No hay transformaciones") || data.includes("URL necesaria")){
-                    alert(JSON.stringify(data));
-                }else{
-                	if (/\d/.test(data)){
-                		var options = JSON.stringify(data).split(",");
-                		window.fetch("http://localhost:3000/api/augmentations/" + options[options.length - 1])
-                		.then(function(response){
-                			response.json().then(function(data){
-                				if(Array.isArray(data)) {
-                				    saveLocalSite(data);
-                				    siteAdaptation = data;
-                				    var index = indexOfCompareByEquals(siteAdaptation, pageUrl, "url");
-                				    if (index < 0) {
-                				        index = indexOfCompareByIncludes(siteAdaptation, pageUrl, "url");
-                				    }
-                				    if (index > -1) {
-                				        executePageAdaptation(index);
-                				    }
-                				    alert("Se ha importado correctamente la configuración.");
-                				}else {
-                				    alert("Los datos ingresados no tienen un formato válido.");
-                				}
-                			});
-                		}).catch(function(err){
-                			alert("Algo salió mal: " + err);
-                		});
-                	}else{
-                		if(Array.isArray(data)) {
-                		    saveLocalSite(data);
-                		    siteAdaptation = data;
-                		    var index = indexOfCompareByEquals(siteAdaptation, pageUrl, "url");
-                		    if (index < 0) {
-                		        index = indexOfCompareByIncludes(siteAdaptation, pageUrl, "url");
-                		    }
-                		    if (index > -1) {
-                		        executePageAdaptation(index);
-                		    }
-                		    alert("Se ha importado correctamente la configuración.");
-                		}else {
-                		    alert("Los datos ingresados no tienen un formato válido.");
-                		}
-                	}
-                }
-
-            });
-		}).catch(function(err) {
-			alert("Algo salió mal: " + err);
-		});
+		var getReqCatalog = new XMLHttpRequest();
+		var urlCatalog = "http://192.168.0.221:3000/api/augmentations/?url=" + myUrl;
+		getReqCatalog.open("GET", urlCatalog, false);
+		getReqCatalog.setRequestHeader("Content-Type", "application/json");
+		getReqCatalog.send();
+		if (getReqCatalog.status == 200 || getReqCatalog.status == 400){
+			var xhrResponse = getReqCatalog.responseText;
+		}
+		/* La longitud debe tener un minimo de datos para asegurar la estructura inicial del Json. */
+		var siteImport = JSON.parse(xhrResponse);
+        if (siteImport.includes("No hay transformaciones") || siteImport.includes("URL necesaria")){
+            alert("" + siteImport);
+        } else{
+            if (/\d/.test(siteImport)){
+	            siteImport+= '';
+				var options = siteImport.split(","); // o siteImport
+			    getReqCatalog = new XMLHttpRequest();
+			    urlCatalog = "http://192.168.0.221:3000/api/augmentations/" + options[options.length - 1];
+			    getReqCatalog.open("GET", urlCatalog, false);
+			    getReqCatalog.setRequestHeader("Content-Type", "application/json");
+			    getReqCatalog.send();
+				if (getReqCatalog.status == 200 || getReqCatalog.status == 400){
+					xhrResponse = getReqCatalog.responseText;
+				}
+			}
+	        siteImport = JSON.parse(xhrResponse);
+	        if(Array.isArray(siteImport)) {
+	            saveLocalSite(siteImport);
+	            siteAdaptation = siteImport;
+	            var index = indexOfCompareByEquals(siteAdaptation, pageUrl, "url");
+	            if (index < 0) {
+	                index = indexOfCompareByIncludes(siteAdaptation, pageUrl, "url");
+	            }
+	            if (index > -1) {
+	                executePageAdaptation(index);
+	            }
+	            alert("Se ha importado correctamente la configuración.");
+	        }
+	        else {
+	            alert("Los datos ingresados no tienen un formato válido.");
+	        }
+        }
 	}
 
 	function saveLocalSite(site){
 		if (typeof(Storage) !== "undefined") {
-			localStorage.setItem("siteAdaptation", JSON.stringify(site));
+			sessionStorage.setItem("siteAdaptation", JSON.stringify(site));
 		}
 		else {
 			alert(localStoragedError);
@@ -190,7 +178,7 @@
 
 	function getLocalSite(){
 		if (typeof(Storage) !== "undefined") {
-			return JSON.parse(localStorage.getItem("siteAdaptation"));
+			return JSON.parse(sessionStorage.getItem("siteAdaptation"));
 		} else {
 			alert(localStoragedError);
 		}
@@ -360,7 +348,7 @@
 				"}</style>");
 				iHead.append("<style> #view-source {position: fixed; display: block; right: 0; bottom: 0; margin-right: 40px; margin-bottom: 40px; z-index: 900; } </style>");
 				iBody.append("<div class='mdl-layout mdl-js-layout mdl-layout--fixed-header'><div class='android-header mdl-layout__header mdl-layout__header--waterfall is-casting-shadow is-compact'>"+
-                "<div aria-expanded='false' role='button' tabindex='0' class='mdl-layout__drawer-button' id='drwrbtn'><i class='material-icons'></i></div> <div class='mdl-layout__header-row'>"+
+                "<div aria-expanded='false' role='button' tabindex='0' class='mdl-layout__drawer-button' id='drwrbtn'><i class='material-icons' style='font-size: 48px; margin-top: -25px'>≡</i></div> <div class='mdl-layout__header-row'>"+
 				"<span class='android-title mdl-layout-title'><div id='header-0' style='color: #8bc34a;'></div></span><div class='android-header-spacer mdl-layout-spacer'></div>"+
 				"<div class='android-navigation-container'><nav class='android-navigation mdl-navigation' id='navigation-0'></nav></div>"+
 				"<span class='android-mobile-title mdl-layout-title'><div id='header-1'></div></span></div></div>"+
@@ -369,8 +357,7 @@
 				"<div class='mdl-mega-footer--top-section'><div class='mdl-mega-footer--right-section'><a class='mdl-typography--font-light' href='#top'>Volver Arriba<i class='material-icons'>expand_less</i></a></div></div>"+
 				"<div class='mdl-mega-footer--middle-section mdl-typography--font-light' id='footer-0'></div>"+
 				"<div class='mdl-mega-footer--bottom-section' id='navigation-2'></div></footer></div>"+
-                "<div class='mdl-layout__obfuscator' id='bfsctr'></div></div>"+
-				"<a href='' target='_blank' id='view-source' class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast'>Ver Original</a>");
+                "<div class='mdl-layout__obfuscator' id='bfsctr'></div></div>");
 				importElement(objectParent["header-0"],"#header-0",iBody);
 				importElement(objectParent["header-0"],"#header-1",iBody);
 				importElement(objectParent["header-0"],"#header-2",iBody);
@@ -517,7 +504,7 @@
 
 	function delLocalSite(){
 		if (typeof(Storage) !== "undefined") {
-			localStorage.removeItem("siteAdaptation");
+			sessionStorage.removeItem("siteAdaptation");
 			siteAdaptation = [];
             location.reload();
 		}
